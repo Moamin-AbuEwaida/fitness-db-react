df1597e3fa526d355e8f2e3cc1c7e98619e97830
Moamin <moamin.salamah@gmail.com>
2023-03-22 14:03:11 +0100

    programs component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Hero from './components/hero/Hero';
+import Programs from './components/programs/Programs';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
